dbfd8682d0c650fe58a1749fbc74c3ecba058534
Andres Reyes <oreyes@cads.com.gt>
2018-05-29 16:37:15 -0600

    se creo archivo para subir imagenes

--- routes/upload.js
+++ routes/upload.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');
 const app = express();
-app.get('/', (req, res) => {
+app.use(fileUpload());
+app.put('/', (req, res, next) => {
+  if (!req.files) {
+    return res.status(400).json({
+      ok: false,
+      mensaje: 'No subio archivo',
+      errors: {message: 'Debe de seleccionar una imagen'}
+    });
+  }
   res.status(200).json({
     ok: true,
     mensaje: 'Peticion realizada exitosamente'
