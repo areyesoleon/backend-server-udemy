83810b3d9a05aa6ee08485ac9fa60713d515a17a
Andres Reyes <oreyes@cads.com.gt>
2018-06-12 16:38:05 -0600

    ajustes de seguridad del usuario

--- routes/login.js
+++ routes/login.js
@@ -110,9 +110,40 @@ app.post('/', (req, res) => {
       ok: true,
       usuario: usuarioDB,
       id: usuarioDB._id,
-      token: token
+      token: token,
+      menu: obtenerMenu(usuarioDB.role)
     });
   });
 });
+
+function obtenerMenu(ROLE){
+  const menu = [
+    {
+      titulo: "Principal",
+      icono: "mdi mdi-gauge",
+      submenu: [
+        { titulo: "Dashboard", url: "/dashboard" },
+        { titulo: "ProgressBar", url: "/progress" },
+        { titulo: "Graficas", url: "/graficas1" },
+        { titulo: "Promesas", url: "/promesas" },
+        { titulo: "Rxjs", url: "/rxjs" }
+      ]
+    },
+    {
+      titulo:'Mantenimientos',
+      icono: 'mdi mdi-folder-lock-open',
+      submenu: [
+        //{titulo: 'Usuarios', url:'/usuarios'},
+        {titulo: 'Hospitales', url:'/hospitales'},
+        {titulo: 'Medicos', url:'/medicos'}
+      ]
+    }
+  ];
+  console.log('ROLE',ROLE);
+  if( ROLE === 'ADMIN_ROLE'){
+    menu[1].submenu.unshift({titulo: 'Usuarios', url:'/usuarios'});
+  }
+  return menu;
+}
 
 module.exports = app;
