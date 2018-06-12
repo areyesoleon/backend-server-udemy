d2df9948446a0e9a034cd1c3fb23c3f6039149e8
Andres Reyes <oreyes@cads.com.gt>
2018-06-12 08:54:52 -0600

    Se agrego busqueda de medico

--- routes/medico.js
+++ routes/medico.js
@@ -29,6 +29,35 @@ app.get('/', (req, res, next) => {
       });
 });
 
+app.get('/:id',(req,res) => {
+  const id = req.params.id;
+  Medico.findById(id)
+  .populate('usuario','nombre email img')
+  .populate('hospital')
+  .exec((err,medico) => {
+    if (err) {
+      return res.status(500).json({
+        ok: false,
+        mensaje: 'Error al buscar medico',
+        errors: err
+      });
+    }
+    if (!medico) {
+      return res.status(400).json({
+        ok: false,
+        mensaje: 'El medico con el id ' + id + ' no existe',
+        errors: {
+          message: 'No existe un medico con ese ID'
+        }
+      });
+    }
+    return res.status(200).json({
+      ok: true,
+      medico: medico
+    });
+  })
+});
+
 app.put('/:id', mdAutenticacion.vericaToken, (req, res) => {
   const id = req.params.id;
   const body = req.body;
